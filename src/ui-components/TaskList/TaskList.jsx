fc182080094f3efb75c4cb9735c3406dbf95c5cd
Kevin Cousin <kevin.cousin44@gmail.com>
2021-09-27 17:15:24 +0200

    Todo list

---
+++ src/ui-components/TaskList/TaskList.jsx
@@ -0,0 +1,56 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import cx from 'classnames';
+
+import './style.scss';
+
+export default class TaskList extends Component {
+  static propTypes = {
+    tasks: PropTypes.array,
+    canArchive: PropTypes.bool,
+    onClickTask: PropTypes.func,
+    onArchiveTask: PropTypes.func,
+    onRemoveTask: PropTypes.func.isRequired,
+  }
+
+  static defaultProps = {
+    tasks: [],
+    canArchive: false,
+  }
+
+  renderTask(task, i) {
+    return (
+      <tr
+        key={`task-${i}`}
+        className="task"
+        onClick={() => this.props.onClickTask(task)}
+      >
+        <td className={cx('TaskText', { done: task.done })}>{task.text}</td>
+        <td className="actions">
+          {this.props.canArchive && (
+            <div onClick={(e) => {
+              e.stopPropagation();
+              this.props.onArchiveTask(task);
+            }}>
+              A
+            </div>
+          )}
+          <div onClick={(e) => {
+            e.stopPropagation();
+            this.props.onRemoveTask(task);
+          }}>X</div>
+        </td>
+      </tr>
+    );
+  }
+
+  render() {
+    return (
+      <table className="TaskList">
+        <tbody>
+          {this.props.tasks.map(this.renderTask.bind(this))}
+        </tbody>
+      </table>
+    );
+  }
+}
--- src/views/TodoList/TodoList.jsx
+++ src/views/TodoList/TodoList.jsx
@@ -1,3 +1,7 @@
+import React, { Component } from 'react';
+
+import TaskList from '../../ui-components/TaskList/TaskList.jsx';
+
 /*
   Créer une vue qui gère une todo list.
 
@@ -7,3 +11,93 @@
   On peut supprimer une tache (elle disparait)
   On peut archiver une tache (elle apparait dans les archives)
 */
+
+import './style.scss';
+
+export default class TodoList extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      editingTask: '',
+      tasks: [
+        { text: 'Aller faire les courses', done: false },
+        { text: 'Apprendre Javascript', done: false },
+      ],
+      archivedTasks: [],
+    };
+  }
+
+  handleCreateTask() {
+    this.setState({
+      editingTask: '',
+      tasks: [
+        ...this.state.tasks,
+        { id: new Date(), text: this.state.editingTask, done: false },
+      ],
+    });
+  }
+
+  handleUpdateTask(task) {
+    this.setState({
+      tasks: this.state.tasks.map((t) => {
+        if (t !== task) return t;
+        return { ...t, done: !t.done };
+      }),
+    });
+  }
+
+  handleArchiveTask(task) {
+    this.setState({
+      tasks: this.state.tasks.filter((t) => t !== task),
+      archivedTasks: [...this.state.archivedTasks, task],
+    });
+  }
+
+  handleRemoveTask(task) {
+    this.setState({
+      tasks: this.state.tasks.filter((t) => t !== task),
+    });
+  }
+
+  handleRemoveArchivedTask(task) {
+    this.setState({
+      archivedTasks: this.state.archivedTasks.filter((t) => t !== task),
+    });
+  }
+
+  render() {
+    return (
+      <div id="TodoList">
+        <div className="EditingTask">
+          <h1>Nouvelle tache</h1>
+          <textarea
+            value={this.state.editingTask}
+            onChange={(e) => this.setState({ editingTask: e.target.value })}
+          />
+          <input
+            type="button"
+            value="Ajouter"
+            onClick={this.handleCreateTask.bind(this)}
+          />
+        </div>
+        <div className="current-tasks tasks">
+          <h1>Taches en cours</h1>
+          <TaskList
+            tasks={this.state.tasks}
+            onClickTask={this.handleUpdateTask.bind(this)}
+            onArchiveTask={this.handleArchiveTask.bind(this)}
+            onRemoveTask={this.handleRemoveTask.bind(this)}
+            canArchive
+          />
+        </div>
+        <div className="archived-tasks tasks">
+          <h1>Taches archivées</h1>
+          <TaskList
+            tasks={this.state.archivedTasks}
+            onRemoveTask={this.handleRemoveArchivedTask.bind(this)}
+          />
+        </div>
+      </div>
+    );
+  }
+}
